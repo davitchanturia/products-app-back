a2fb45f5b4ec29d3ab4c82c29f25c5ea4211b8d7
davit.chanturia <chanturiadato@gmail.com>
2024-05-01 18:18:00 +0400

    feat: adding new endpoint to fetch certain product

--- app.js
+++ app.js
@@ -57,6 +57,23 @@ app.get('/products', (req, res) => {
     });
 });
 
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id; 
+
+    db.get("SELECT * FROM products WHERE id = ?", [id], (err, row) => {
+        if (err) {
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            if (row) {
+                res.json(row);
+            } else {
+                res.status(404).json({ error: 'Product not found' });
+            }
+        }
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
