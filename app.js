98dee6cd0b367021d479f99ec9aacac388c8631c
davit.chanturia <chanturiadato@gmail.com>
2024-05-01 12:29:14 +0400

    feat: return paginated data on products get endpoint

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
 const express = require('express');
+const products = require('./data');
 const sqlite3 = require('sqlite3').verbose(); 
 
 const app = express();
 const port = 3000; 
 const db = new sqlite3.Database('./database.sqlite');
-
-const products = [
-    { name: 'Iphone 12', price: '1300', 
-        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0127451_apple-iphone-12-64gb-black_550.png', 
-        image_two: 'https://s3.zoommer.ge/zoommer-images/thumbs/0127452_apple-iphone-12-64gb-black_550.png', 
-        image_three: null
-    },
-    { name: 'Iphone 13', price: '1400', 
-        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0154537_apple-iphone-13-128gb-red_550.jpeg',
-        image_two: 'https://s3.zoommer.ge/zoommer-images/thumbs/0154538_apple-iphone-13-128gb-red_550.jpeg', 
-        image_three: null
-    },
-    { name: 'Iphone 15', price: '1500', 
-        image_one: 'https://s3.zoommer.ge/site/1ced6132-0964-4c71-8898-58fb9dc54fd3_Thumb.jpeg', 
-        image_two: 'https://s3.zoommer.ge/site/09282735-add9-4c57-898c-7bfbd5c4885c_Thumb.jpeg', 
-        image_three: 'https://s3.zoommer.ge/site/f3df4a35-1891-4d0e-a8c6-98fe56067f36_Thumb.jpeg' 
-    }
-];
 
 const allowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
@@ -48,11 +31,28 @@ db.serialize(() => {
 });
 
 app.get('/products', (req, res) => {
-    db.all("SELECT * FROM products", (err, rows) => {
+    const pageSize = 5; 
+    const page = parseInt(req.query.page) || 1; 
+
+    // Query the database to count the total number of records
+    db.get("SELECT COUNT(*) as count FROM products", (err, row) => {
         if (err) {
             res.status(500).json({ error: 'Internal server error' });
         } else {
-            res.json(rows);
+            const totalRecords = row.count; 
+            const totalPages = Math.ceil(totalRecords / pageSize);
+
+            const offset = (page - 1) * pageSize; 
+
+            // Query the database to fetch paginated products
+            db.all(`SELECT * FROM products LIMIT ? OFFSET ?`, [pageSize, offset], (err, rows) => {
+                if (err) {
+                    console.error(err.message);
+                    res.status(500).json({ error: 'Internal server error' });
+                } else {
+                    res.json({ totalPages, currentPage: page, pageSize, products: rows });
+                }
+            });
         }
     });
 });
---
+++ data.js
@@ -0,0 +1,56 @@
+const products = [
+    { name: 'Iphone 12', price: '1300', 
+        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0127451_apple-iphone-12-64gb-black_550.png', 
+        image_two: 'https://s3.zoommer.ge/zoommer-images/thumbs/0127452_apple-iphone-12-64gb-black_550.png', 
+        image_three: null
+    },
+    { name: 'Iphone 13', price: '1400', 
+        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0154537_apple-iphone-13-128gb-red_550.jpeg',
+        image_two: 'https://s3.zoommer.ge/zoommer-images/thumbs/0154538_apple-iphone-13-128gb-red_550.jpeg', 
+        image_three: null
+    },
+    { name: 'Apple MacBook Air', price: '3500', 
+        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0196691_apple-macbook-air-153-inch-2023-mqkq3lla-m2-chip-8gb512gb-ssd-space-grey_550.jpeg', 
+        image_two: null, 
+        image_three: null
+    },
+    { name: 'Apple MacBook Pro 14 inch', price: '4500', 
+        image_one: 'https://s3.zoommer.ge/site/f0d01917-6123-4488-ad5c-e84ee0efe688_Thumb.jpeg', 
+        image_two: 'https://s3.zoommer.ge/site/50eeafad-5f5c-4248-94b9-6bf98c04ecce_Thumb.jpeg', 
+        image_three: 'https://s3.zoommer.ge/site/a7882018-5b46-46d7-9769-f4a942699cf6_Thumb.jpeg' 
+    },
+    { name: 'Samsung Galaxy S24 Ultra', price: '3000', 
+        image_one: 'https://s3.zoommer.ge/site/388ed075-6c4f-40ee-bcc5-838a420fc625_Thumb.jpeg', 
+        image_two: 'https://s3.zoommer.ge/site/cfc408b2-d145-4a87-9d34-19ab775c9854_Thumb.jpeg', 
+        image_three: null
+    },
+    { name: 'Xiaomi Redmi Note 13 Pr', price: '799', 
+        image_one: 'https://s3.zoommer.ge/site/5a187779-ce44-4ef1-9f9e-c8f8b7437b55_Thumb.jpeg', 
+        image_two: null, 
+        image_three: null
+    },
+    { name: 'HP Victus 15', price: '3800', 
+        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0192424_hp-victus-15-84j96ea-intel-core-i7-13700h-nvidia-geforce-rtx-4050-6gb-16gb-ram-ssd-512gb-free-dos-le_550.jpeg', 
+        image_two: null, 
+        image_three: null
+    },
+    { name: 'Lenovo LOQ 15', price: '2900', 
+        image_one: 'https://s3.zoommer.ge/site/d66bd763-bbd7-4d59-851a-f96e0ee916de_Thumb.jpeg', 
+        image_two: 'https://s3.zoommer.ge/site/ff8ea049-7b68-4e4e-8a53-5ca172b26359_Thumb.jpeg', 
+        image_three: null
+    },
+    { name: 'Apple iPad Pro 11 2022 4th Gen', price: '2600', 
+        image_one: 'https://s3.zoommer.ge/zoommer-images/thumbs/0182432_apple-ipad-pro-11-2022-4th-gen-128gb-wi-fi-space-grey_550.jpeg', 
+        image_two: 'https://s3.zoommer.ge/zoommer-images/thumbs/0182433_apple-ipad-pro-11-2022-4th-gen-128gb-wi-fi-space-grey_550.jpeg', 
+        image_three: null
+    },
+
+    { name: 'Samsung Galaxy A25', price: '550', 
+        image_one: 'https://s3.zoommer.ge/site/2bac5e4f-68d8-4fea-b2fd-b01fbf363dbb_Thumb.jpeg', 
+        image_two: null, 
+        image_three: null
+    }
+];
+
+module.exports = products;
+
